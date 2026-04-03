02ac3601de9eb240194d0eb1bacd56e2cc01dc68
Alpheus <26622568+alpheusmtx@users.noreply.github.com>
2026-04-03 15:21:36 +0800

    feat: add response caching

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,6 +1,7 @@
 import type { YogaServerInstance } from "graphql-yoga";
 
 import { maxTokensPlugin } from "@escape.tech/graphql-armor-max-tokens";
+import { useResponseCache } from "@graphql-yoga/plugin-response-cache";
 import { useContentEncoding } from "@whatwg-node/server";
 import { createYoga } from "graphql-yoga";
 
@@ -33,6 +34,11 @@ const yogaServer: YogaServerInstance<object, object> = createYoga({
         // token limitation
         maxTokensPlugin({
             n: 1000,
+        }),
+        // response caching
+        useResponseCache({
+            session: () => null,
+            ttl: 1000 * 60 * 60, // 1 hour
         }),
         // response compression
         useContentEncoding(),
